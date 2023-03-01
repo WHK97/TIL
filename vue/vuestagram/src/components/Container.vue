1fc549af4fb4033e4aa5baf905909e5d797e229f
WHK97 <woo3681@naver.com>
2023-03-01 21:53:08 +0900

    vue

---
+++ vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :userData = "userData[i]" v-for="(a,i) in userData" :key="i"/>
+  </div>
+ 
+</template>
+
+<script>
+import Post from "./Post.vue"
+export default {
+  name: "ContainerPage",
+  props:{
+    userData: Array,
+  },
+  components:{
+    Post
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/vuestagram/src/router.js
@@ -0,0 +1,19 @@
+// import { createWebHistory, createRouter } from "vue-router";
+// import Post from "./components/Post.vue"
+// const routes = [
+//   {
+//     path: "/",
+    
+//   },
+//   {
+//     path: "/post  ",
+//     component: Post,
+//   },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// export default router;
